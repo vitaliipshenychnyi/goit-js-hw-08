f1404be6b6e1d6aa7c3a1d2a5154767d38434e87
VitaliiPshenychniy <4kvaap@gmail.com>
2023-03-28 12:30:51 +0300

    First

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+// Module with Babel tranformation
+import SimpleLightbox from 'simplelightbox';
+// Plain ES Module without Babel
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+// Створення і рендер розмітки на підставі масиву даних galleryItems
+const galleryListEl = document.querySelector('.gallery');
+galleryListEl.insertAdjacentHTML('beforeend', createGalleryItem(galleryItems));
+
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}">
+      </a>
+      </li>`
+    )
+    .join('');
+}
+
+// Ініціалізація бібліотеки SimpleLightbox
+const slideOriginalImg = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
