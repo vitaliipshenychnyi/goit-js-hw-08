03b1188c361295a22172b5aebfba4df0f457e88f
VitaliiPshenychniy <4kvaap@gmail.com>
2023-03-29 09:24:37 +0300

    second

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// Відстеження оновлення часу та збереження у локадьне сховище
+// із затримкою в 1 секунду, використовуючи бібліотеку lodash
+const throttle = require('lodash.throttle');
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+function saveCurrentTime({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', Math.round(seconds));
+}
+
+// Відновлення програвання зі збереженої позиції
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+const formEl = document.querySelector('.feedback-form');
+const storageKey = 'feedback-form-state';
 
+// Зчитування даних з полів та збереження їх у вигляді об'єкту
+formEl.addEventListener('input', saveDataForm);
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+function saveDataForm() {
+  
+    formData.email = formEl.elements.email.value;
+    formData.message = formEl.elements.message.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  
+}
+
+// Заповнення полів форми даними зі сховища
+document.addEventListener('DOMContentLoaded', loadDataForm);
+let savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+function loadDataForm() {
+  //   if (localStorage.length) {
+  //     formEl.elements.email.value = savedData.email;
+  //     formEl.elements.message.value = savedData.message;
+  //   } else {
+  //     formEl.elements.email.value = '';
+  //     formEl.elements.message.value = '';
+  //   }
+}
+
+// -----------
+// console.log(savedData);
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log(formData);
+  formEl.reset();
+  localStorage.clear();
+}
