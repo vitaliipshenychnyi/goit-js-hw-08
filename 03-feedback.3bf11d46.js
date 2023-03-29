10017dc702a5431e458523bf993f1b8ebc3695ea
vitaliipshenychnyi <117928970+vitaliipshenychnyi@users.noreply.github.com>
2023-03-29 10:34:05 +0000

    Deploying to gh-pages from @ vitaliipshenychnyi/goit-js-hw-08@ac5f684b65f890849ba8ceef98a73b07bbbdbb7b 🚀

--- 03-feedback.3bf11d46.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form");e.addEventListener("input",(function(){t.email=e.elements.email.value,t.message=e.elements.message.value,localStorage.setItem("feedback-form-state",JSON.stringify(t))}));const t={email:"",message:""};document.addEventListener("DOMContentLoaded",(function(){}));JSON.parse(localStorage.getItem("feedback-form-state"));e.addEventListener("submit",(function(a){a.preventDefault(),console.log(t),e.reset(),localStorage.clear()}));
-//# sourceMappingURL=03-feedback.3bf11d46.js.map
