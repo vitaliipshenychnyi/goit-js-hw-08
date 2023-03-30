be0f6cd0e6d6ba3518c2297bcc1c75daa8519d35
VitaliiPshenychnyi <117928970+vitaliipshenychnyi@users.noreply.github.com>
2023-03-30 13:01:57 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,36 +1,34 @@
 import Player from '@vimeo/player';
 
+const throttleTime = require('lodash.throttle');
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const storageKeyTime = 'videoplayer-current-time';
 
 // Відстеження оновлення часу та збереження у локадьне сховище
 // із затримкою в 1 секунду, використовуючи бібліотеку lodash
-const throttle = require('lodash.throttle');
-
-player.on('timeupdate', throttle(saveCurrentTime, 1000));
+player.on('timeupdate', throttleTime(saveCurrentTime, 1000));
 
 function saveCurrentTime({ seconds }) {
   localStorage.setItem(storageKeyTime, Math.round(seconds));
 }
 
 // Відновлення програвання зі збереженої позиції
-// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-
 player
-  .setCurrentTime(localStorage.getItem(storageKeyTime))
+  .setCurrentTime(10)
   .then(function (seconds) {
-    seconds = 10;
+    seconds = localStorage.getItem(storageKeyTime)
+      ? player.setCurrentTime(localStorage.getItem(storageKeyTime))
+      : player.setCurrentTime(0);
   })
   .catch(function (error) {
     switch (error.name) {
       case 'RangeError':
-        console.log(
-          'The time was less than 0 or greater than the video’s duration'
-        );
+        player.setCurrentTime(0);
         break;
 
       default:
+        player.setCurrentTime(0);
         break;
     }
   });
